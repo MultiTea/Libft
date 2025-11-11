27692ddaee3ed9251bcd115f517e9fab0b89992c
Lucas Bolea <lbolea@f6r7s14.clusters.42paris.fr>
2025-11-11 19:19:51 +0100

    Adding ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lbolea <lbolea@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 17:26:40 by lbolea            #+#    #+#             */
-/*   Updated: 2025/11/11 18:03:52 by lbolea           ###   ########.fr       */
+/*   Updated: 2025/11/11 19:06:49 by lbolea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,48 +17,60 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*d;
 	unsigned char	*s;
-	unsigned char	*tmp;
-	size_t			i;
 
 	d = dest;
 	s = (unsigned char *)src;
-	tmp[n] = 0;
-	i = 0;
-	while (i < n)
+	if (n == 0 || dest == src)
+		return (dest);
+	if (d > s && d < s + n)
 	{
-		tmp[i] = s[i];
-		i++;
+		while (n--)
+			d[n] = s[n];
 	}
-	while (i != 0)
+	else
 	{
-		d[i] = tmp[i];
-		i--;
+		while (n--)
+			*d++ = *s++;
 	}
 	return (dest);
 }
 
 void	test1(void)
 {
-	char	str1[10] = "À droite";
-	char	str2[10] = "À gauche";
-	char	str3[10] = "À droite";
-	char	str4[10] = "À gauche";
+	char	str1[10] = "ABCDEFG";
+	char	str2[10] = "ABCDEFGHI";
+	char	str3[10] = "ABCDEFG";
+	char	str4[10] = "ABCDEFGHI";
 	int		buff;
 
-	str1[9] = '\0';
-	str2[9] = '\0';
-	str3[9] = '\0';
-	str4[9] = '\0';
-	buff = 8;
-	printf("%s -> %s\n", str1, str2);
-	printf("%s -> %s\n", str3, str4);
-	ft_memmove(str1 + 2, str2, buff);
-	memmove(str3 + 2, str4, buff);
-	printf("%s <- %s\n", str1, str2);
-	printf("%s <- %s\n", str3, str4);
+	buff = 9;
+	printf("	2 DIFFERENTS STRINGS ");
+	ft_memmove(str1, str2, buff);
+	memmove(str3, str4, buff);
+	if (!(strcmp(str1, str3)))
+		printf("\033[0;32mOK!\n\033[0m");
+	else
+		printf("\033[0;31mKO\n\033[0m");
+}
+
+void	test2(void)
+{
+	char	str1[20] = "ABCDEFGHIJ";
+	char	str2[20] = "ABCDEFGHIJ";
+	int		buff;
+
+	buff = 5;
+	printf("	SAME STRING OVERLAP ");
+	ft_memmove(str1 + buff, str1, buff);
+	memmove(str2 + buff, str2, buff);
+	if (!(memcmp(str1, str2, 20)))
+		printf("\033[0;32mOK!\n\033[0m");
+	else
+		printf("\033[0;31mKO\n\033[0m");
 }
 
 int	main(void)
 {
 	test1();
+	test2();
 }
